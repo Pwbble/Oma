5c42733402050cf461937bddf5dce135b60aed81
Pwbble <cowtorode@gmail.com>
2022-05-23 22:38:14 -0400

    Message Fixes

--- src/com/oma/commands/Worlds.java
+++ src/com/oma/commands/Worlds.java
@@ -2,6 +2,7 @@ package com.oma.commands;
 
 import com.oma.enums.Message;
 import com.oma.handlers.MessageHandler;
+import com.oma.handlers.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.command.Command;
@@ -14,9 +15,9 @@ public class Worlds implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String string, String[] arg) {
         if (!(sender.hasPermission("oma.command.worlds") || sender.hasPermission("oma.*")))
             return MessageHandler.createInstance().sendMessage(Message.INVALID_PERMISSION, sender);
-        for (World worlds : Bukkit.getWorlds())
-            MessageHandler.createInstance().sendMessage(Message.WORLDS,
-                    s -> s.replace("%worlds%", worlds.getName()), sender);
-        return true;
+        StringBuilder eval = new StringBuilder();
+        for (World world : Bukkit.getWorlds()) eval.append(world.getName()).append(Utils.chatColor("&7, &b"));
+        return MessageHandler.createInstance().sendMessage(Message.WORLDS,
+                s -> s.replace("%worlds%", eval), sender);
     }
 }
